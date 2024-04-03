2ca8c49d99b1d2f962f1fd1fb72670b60d0c9014
Taras Hreshchyshyn <hreshchyshyn.t@gmail.com>
2024-04-04 02:23:07 +0300

    shortcuts for saving file, inserting empty lines and duplicating line

--- init.lua
+++ init.lua
@@ -102,7 +102,7 @@ vim.g.have_nerd_font = false
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -189,6 +189,18 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- TODO: Save file, format file, optimize imports, duplicate selected text (or line if nothing selected)
+-- TODO: delete word in edit mode
+-- norremap <C-s> :w<CR>
+vim.keymap.set('i', '<C-s>', '<C-O>:w<CR>', { desc = 'Save current file', noremap = true, silent = true })
+vim.keymap.set('n', '<C-s>', ':w<CR>', { desc = 'Save current file', noremap = true, silent = true })
+vim.keymap.set('i', '<S-Enter>', '<Esc>o', { desc = 'insert empty line below and move cursor on it', noremap = true, silent = true })
+vim.keymap.set('i', '<C-Enter>', '<Esc>O', { desc = 'insert empty line above and move cursor on it', noremap = true, silent = true })
+vim.keymap.set('n', '<C-d>', 'yyp', { noremap = true, silent = true })
+--
+--
+--
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
