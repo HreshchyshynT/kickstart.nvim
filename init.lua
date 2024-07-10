fa49e1483d73a7280ca909d826f37e3c003e0b6e
Taras Hreshchyshyn <hreshchyshyn.t@gmail.com>
2024-07-10 11:27:40 +0300

    enabled project related configs with exrc

--- init.lua
+++ init.lua
@@ -161,6 +161,9 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- Enable project related configs
+vim.opt.exrc = true
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
