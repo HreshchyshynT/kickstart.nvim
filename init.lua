e4233e67c9bcce52d461872210aad8254acec9be
Taras Hreshchyshyn <hreshchyshyn.t@gmail.com>
2024-07-12 17:30:13 +0300

    keybinding for method signature

--- init.lua
+++ init.lua
@@ -535,6 +535,7 @@ require('lazy').setup({
           --  For example, in C this would take you to the header.
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
+          vim.keymap.set('i', '<C-p>', vim.lsp.buf.signature_help, { desc = 'Display method signature help in the insert mode', noremap = true })
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
           --    See `:help CursorHold` for information about when this is executed
