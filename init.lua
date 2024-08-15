433521ebdd70c314ebfb1fc1c4165129c6509032
Taras Hreshchyshyn <hreshchyshyn.t@gmail.com>
2024-08-15 10:45:38 +0300

    added vim be good

--- init.lua
+++ init.lua
@@ -931,6 +931,10 @@ require('lazy').setup({
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
   { import = 'custom.plugins' },
+  {
+    'ThePrimeagen/vim-be-good',
+    lazy = false,
+  },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
